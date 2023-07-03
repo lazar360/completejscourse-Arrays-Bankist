924a4cc23d777a785eea4db2a82a176a4e251195
NicolasGautier <nicolas.gautier35135@gmail.com>
2023-07-03 10:11:57 +0200

    maj script

--- script.js
+++ script.js
@@ -75,9 +75,6 @@ const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
 
-let arr = ['a', 'b', 'c', 'd', 'e'];
-
-
 // SLICE
 // console.log(arr.slice(2));
 // console.log(arr.slice(2, 4));
@@ -89,5 +86,11 @@ let arr = ['a', 'b', 'c', 'd', 'e'];
 
 // SPLICE 
 //console.log(arr.splice(2));
-arr.splice(-1)
-console.log(arr);
+// arr.splice(-1);
+// arr.splice(1,2);
+// console.log(arr);
+
+// REVERSE
+let arr = ['a', 'b', 'c', 'd', 'e'];
+const arr2 = ['j', 'i', 'h', 'g', 'f'];
+console.log(arr2.reverse());
