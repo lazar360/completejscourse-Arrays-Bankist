37bf7e4ffa99e356310d334f580b54a01b48a447
NicolasGautier <nicolas.gautier35135@gmail.com>
2023-07-03 10:30:04 +0200

    maj script

--- script.js
+++ script.js
@@ -91,15 +91,25 @@ const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 // console.log(arr);
 
 // REVERSE
-let arr = ['a', 'b', 'c', 'd', 'e'];
-const arr2 = ['j', 'i', 'h', 'g', 'f'];
-console.log(arr2.reverse());
-console.log(arr2);
+// let arr = ['a', 'b', 'c', 'd', 'e'];
+// const arr2 = ['j', 'i', 'h', 'g', 'f'];
+// console.log(arr2.reverse());
+// console.log(arr2);
 
 // CONCAT
-const letters = arr.concat(arr2);
-console.log(letters);
-console.log([...arr, ...arr2]);
+// const letters = arr.concat(arr2);
+// console.log(letters);
+// console.log([...arr, ...arr2]);
 
 // JOIN
-console.log(letters.join(' - '));
+// console.log(letters.join(' - '));
+
+const arr = [23, 11, 64];
+console.log(arr[0]);
+console.log(arr.at(0));
+
+// getting the last array element
+console.log(arr[arr.length - 1]);
+console.log(arr.slice(-1)[0]);
+console.log(arr.at(-1));
+
