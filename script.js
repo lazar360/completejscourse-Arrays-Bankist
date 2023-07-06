04da5656a55ec49ace7318f422497acaec78c1e6
NicolasGautier <nicolas.gautier35135@gmail.com>
2023-07-06 14:12:53 +0200

    maj script

--- script.js
+++ script.js
@@ -196,7 +196,6 @@ btnTransfer.addEventListener('click', function (e) {
 
 btnClose.addEventListener('click', function (e) {
   e.preventDefault();
-
   if (
     inputCloseUsername.value === currentAccount.username &&
     Number(inputClosePin.value) === currentAccount.pin
@@ -204,8 +203,8 @@ btnClose.addEventListener('click', function (e) {
     const index = accounts.findIndex(
       acc => acc.username === currentAccount.username
     );
-    console.log(index);
-    // accounts.splice(index, 1);
+    accounts.splice(index, 1);
+    containerApp.style.opacity = 0;
   }
 });
 
