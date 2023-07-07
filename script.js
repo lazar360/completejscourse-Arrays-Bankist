e8f1c28cc97990d210817cac2bef5c45798a983f
NicolasGautier <nicolas.gautier35135@gmail.com>
2023-07-07 16:05:48 +0200

    maj script

--- script.js
+++ script.js
@@ -499,7 +499,7 @@ console.log(movements);
 // return < 0, A, B
 // return > 0, B, A
 
-// Ascending 
+// Ascending
 
 // movements.sort((a, b) => {
 //   if (a > b) return 1;
@@ -510,9 +510,10 @@ movements.sort((a, b) => a - b);
 console.log(movements);
 
 // Descending
-movements.sort((a, b) => {
-  if (a > b) return -1;
-  if (a < b) return 1;
-});
+// movements.sort((a, b) => {
+//   if (a > b) return -1;
+//   if (a < b) return 1;
+// });
 
+movements.sort((a, b) => b - a);
 console.log(movements);
