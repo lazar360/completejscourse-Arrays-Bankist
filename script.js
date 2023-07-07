602f6cc64a17f53604a93b068b8bfd0320c5c6cd
NicolasGautier <nicolas.gautier35135@gmail.com>
2023-07-07 10:07:54 +0200

    maj script

--- script.js
+++ script.js
@@ -475,8 +475,15 @@ console.log(arr.flat());
 const arrDeep = [[[1, 2], 3, 4], [5, 6, 7], 8, 9];
 console.log(arrDeep.flat(2));
 
+// flat
 const overallBalance = accounts
   .map(acc => acc.movements)
   .flat()
   .reduce((acc, mov) => acc + mov, 0);
 console.log('overallBalance', overallBalance);
+
+// flatMap : one level deep
+const overallBalance2 = accounts
+  .flatMap(acc => acc.movements)
+  .reduce((acc, mov) => acc + mov, 0);
+console.log('overallBalance2', overallBalance2);
