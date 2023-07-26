b4c2ff897a961fd6c9e74e75e87e925b0214d74c
nicolas.gautier35135@gmail.com <nicolas.gautier35135@gmail.com>
2023-07-26 12:33:17 +0200

    maj script

--- script.js
+++ script.js
@@ -307,3 +307,6 @@ console.log(Math.max(1,23, 42, '56'));
 console.log(Math.min(1,23, 42, '56'));
 console.log(Math.sqrt(25));
 console.log(Math.PI * Number.parseFloat(10)**2);
+console.log(Math.trunc(Math.random()*6+1));
+const randInt = (min, max) => Math.trunc(Math.random()*(max - min) + min);
+console.log(randInt(7,12));
